e5521bfe2c2d55ffe1255d6879f805e84676019c
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-07-23 14:11:38 +0530

    aaa

--- app1.js
+++ app1.js
@@ -17,6 +17,58 @@ function getdetails() {
                 img.setAttribute('src',response.Poster);
 
 
+            let div1=document.createElement('div');
+            div1.classList.add("col-md-8");
+
+            let h3=document.createElement('h3');
+            let strong=document.createElement('strong');
+            strong.innerText="Title";
+            // h2.appendChild(strong);
+            // h2.appendChild(response.Title);
+            h3.append(strong);
+            h3.append(': '+response.Title);
+
+            let ul=document.createElement('ul');
+            let li1=document.createElement('li');
+            let li2=document.createElement('li');
+            let li3=document.createElement('li');
+            let li4=document.createElement('li');
+            let li5=document.createElement('li');
+            let li6=document.createElement('li');
+            let li7=document.createElement('li');
+            let li8=document.createElement('li');
+
+            li1.innerText=response.Genre;
+
+            li2.innerText=response.Released;
+            li3.innerText=response.Rated;
+            li4.innerText=response.imdbRating;
+            li5.innerText=response.Actors;
+            li6.innerText=response.Director;
+            li7.innerText=response.Writer;
+            li8.innerText=response.Plot;
+            ul.classList.add("list-group")
+            ul.appendChild(li1);
+            ul.appendChild(li2);
+            ul.appendChild(li3);
+            ul.appendChild(li4);
+            ul.appendChild(li5);
+            ul.appendChild(li6);
+            ul.appendChild(li7);
+            ul.appendChild(li8);
+         let p=   document.createElement('p');
+
+
+
+
+            div1.appendChild(h3);
+            div1.appendChild(ul);
+            div1.appendChild(p);
+            div1.appendChild(p);
+            div1.appendChild(p);
+
+
+
 
 
 
@@ -26,6 +78,7 @@ function getdetails() {
 
 
                 document.querySelector('.row').appendChild(img);
+                document.querySelector('.row').appendChild(div1);
 
 
 
